c78c0a59937ccfb21f23aa8935aadbc027b0c4be
Samuel Galo <samuelgalo1625@gmail.com>
2024-02-03 12:33:18 -0600

    Commit1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let btncal = document.getElementById('cal');
+let btneli = document.getElementById('eliminar');
+
+
+function calcular(){
+let n1 = document.getElementById('nota1').value
+let n2 = document.getElementById('nota2').value
+let n3 = document.getElementById('nota3').value
+
+alert("funciona we")
+
+}
+
